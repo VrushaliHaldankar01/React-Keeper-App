097c6ed73d9b046366965b1d3b88c44b3f40a2a7
Vrushali <vrushalihaldankar01@gmail.com>
2022-10-30 17:47:22 +0530

    Updates

---
+++ precache-manifest.d9177a8841e3f4193b35c19d994678dc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9da0e8dc7b88976211fab56daa93cd",
+    "url": "/React-Keeper-App/index.html"
+  },
+  {
+    "revision": "a2d098e787a1039d91b2",
+    "url": "/React-Keeper-App/static/js/2.c63736d0.chunk.js"
+  },
+  {
+    "revision": "7771f4573076e1d92fc2",
+    "url": "/React-Keeper-App/static/js/main.5c022825.chunk.js"
+  },
+  {
+    "revision": "2983343ac7f02c21ecc6",
+    "url": "/React-Keeper-App/static/js/runtime-main.321da94e.js"
+  }
+]);
